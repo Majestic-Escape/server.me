b3a6697d9c2fb62b98216de2b31a492e353e78a4
aditya <adityaparuleker.cr@gmail.com>
2026-01-06 15:01:07 +0530

    Removed unused cron

--- api/cron/payment/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  console.log("Cron job executed");
-
-  return new Response(
-    JSON.stringify({ success: true, message: "Cron ran successfully" }),
-    { status: 200 }
-  );
-}

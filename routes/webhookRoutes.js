08daba2fe22756f2d9e440338c7bc7dbc0ffb38e
aditya <adityaparuleker.cr@gmail.com>
2026-06-15 17:54:02 +0530

    Changed post to get webhook payout

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 
 // ✅ NO body parser here - webhooks need raw body
-router.post("/payout/update", paymentController.update);
+router.get("/payout/update", paymentController.update);
 
 module.exports = router;
